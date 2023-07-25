cf2b5ac2cc0015203f3d95a7a444364d17f5b52d
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-07-25 23:22:37 +0800

    Initialize project

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,20 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .on_window_event(|e| {
+            if let tauri::WindowEvent::Resized(_) = e.event() {
+                std::thread::sleep(std::time::Duration::from_nanos(1000));
+            }
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
