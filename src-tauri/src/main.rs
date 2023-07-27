a41c8871e28d43ecc1664d6553e64cf54d09418f
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-07-27 13:51:59 +0800

    Added project ID and formatted Rust code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
 
 mod refresh_wallpaper;
 
-use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent, SystemTray};
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -14,16 +14,15 @@ fn main() {
     // Define menu item entries
     let refresh = CustomMenuItem::new("refresh".to_string(), "Refresh wallpaper");
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    
+
     // Populate tray menu with entries
-    let tray_menu = SystemTrayMenu::new() 
-    .add_item(refresh)
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(quit);
-    
-    let system_tray = SystemTray::new()
-        .with_menu(tray_menu);
-    
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(refresh)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
         .on_window_event(|e| {
             if let tauri::WindowEvent::Resized(_) = e.event() {
@@ -34,35 +33,33 @@ fn main() {
         .system_tray(system_tray)
         .on_system_tray_event(|_app, event| match event {
             SystemTrayEvent::LeftClick {
-              position: _,
-              size: _,
-              ..
+                position: _,
+                size: _,
+                ..
             } => {
-              println!("system tray received a left click");
+                println!("system tray received a left click");
             }
             SystemTrayEvent::RightClick {
-              position: _,
-              size: _,
-              ..
+                position: _,
+                size: _,
+                ..
             } => {
-              println!("system tray received a right click");
+                println!("system tray received a right click");
             }
-            SystemTrayEvent::MenuItemClick { id, .. } => {
-              match id.as_str() {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
                 "refresh" => {
                     match tauri::async_runtime::block_on(refresh_wallpaper::refresh_wallpaper()) {
                         Ok(()) => (),
                         Err(_) => eprintln!("Error refreshing"),
                     };
-                },
+                }
                 "quit" => {
-                  std::process::exit(0);
+                    std::process::exit(0);
                 }
                 _ => {}
-              }
-            }
+            },
             _ => {}
-          })
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/refresh_wallpaper.rs
+++ src-tauri/src/refresh_wallpaper.rs
@@ -1,62 +1,67 @@
+use image::{DynamicImage, EncodableLayout, GenericImageView, ImageOutputFormat};
+use std::{collections::HashMap, io::Cursor, path::PathBuf};
 use tauri::api::path::cache_dir;
-use std::{collections::HashMap, path::PathBuf, io::Cursor};
-use image::{GenericImageView, DynamicImage, EncodableLayout, ImageOutputFormat};
 
 pub fn sanitize_image_dimensions(image_obj: &DynamicImage) -> Option<DynamicImage> {
-  // Get image dimensions
-  let (width, height) = image_obj.dimensions();
-  // Check if height is larger than width
-  if height > width {
-      // Define the coordinates and dimensions of the crop rectangle
-      let x = 0;
-      let y = 0;
-      let new_height = height / 2;
-      
-      // Crop the image
-      return Some(image_obj.crop_imm(x, y, width, new_height));
-  }
-  else {
-    return None;
-  }
+    // Get image dimensions
+    let (width, height) = image_obj.dimensions();
+    // Check if height is larger than width
+    if height > width {
+        // Define the coordinates and dimensions of the crop rectangle
+        let x = 0;
+        let y = 0;
+        let new_height = height / 2;
+
+        // Crop the image
+        return Some(image_obj.crop_imm(x, y, width, new_height));
+    } else {
+        return None;
+    }
 }
 
 pub async fn new_wallpaper_url(client: &reqwest::Client) -> Result<String, reqwest::Error> {
-  let result = client.get("https://api.waifu.pics/sfw/waifu")
-  .send()
-  .await?
-  .text()
-  .await?;
-  let decoded_result: &String = &serde_json::from_str::<HashMap::<String, String>>(&result).unwrap()["url"];
-  return Ok(decoded_result.clone());
+    let result = client
+        .get("https://api.waifu.pics/sfw/waifu")
+        .send()
+        .await?
+        .text()
+        .await?;
+    let decoded_result: &String =
+        &serde_json::from_str::<HashMap<String, String>>(&result).unwrap()["url"];
+    return Ok(decoded_result.clone());
 }
 
-pub async fn download_file_from_url(client: &reqwest::Client, url: String, path: PathBuf) -> Result<PathBuf, reqwest::Error> {
-  let result = client.get(url)
-  .send()
-  .await?
-  .bytes()
-  .await?;
-  let img = image::load_from_memory(result.as_bytes()).unwrap();
-  let mut image_bytes: Vec<u8> = Vec::new();
-  let result_image = sanitize_image_dimensions(&img).unwrap_or(img);
-  result_image.write_to(&mut Cursor::new(&mut image_bytes), ImageOutputFormat::Png).unwrap();
-  match std::fs::write(&path, image_bytes) {
-    Ok(_) => return Ok(path),
-    Err(x) => panic!("{}", x)
-  };
+pub async fn download_file_from_url(
+    client: &reqwest::Client,
+    url: String,
+    path: PathBuf,
+) -> Result<PathBuf, reqwest::Error> {
+    let result = client.get(url).send().await?.bytes().await?;
+    let img = image::load_from_memory(result.as_bytes()).unwrap();
+    let mut image_bytes: Vec<u8> = Vec::new();
+    let result_image = sanitize_image_dimensions(&img).unwrap_or(img);
+    result_image
+        .write_to(&mut Cursor::new(&mut image_bytes), ImageOutputFormat::Png)
+        .unwrap();
+    match std::fs::write(&path, image_bytes) {
+        Ok(_) => return Ok(path),
+        Err(x) => panic!("{}", x),
+    };
 }
 
 pub async fn refresh_wallpaper() -> Result<(), ()> {
-  println!("Refreshing wallpaper in function...");
-  let client = reqwest::Client::new();
-  let dl_url = new_wallpaper_url(&client).await.unwrap();
-  let file_name = dl_url.split("/").collect::<Vec<&str>>();
-  let image_file = cache_dir().unwrap().join(file_name[file_name.len() - 1]);
-  let downloaded_file = download_file_from_url(&client, dl_url, image_file).await.unwrap();
-  wallpaper::set_from_path(downloaded_file.display().to_string().as_str()).unwrap();
-  match wallpaper::set_mode(wallpaper::Mode::Crop) {
-    Ok(()) => (),
-    Err(_) => eprintln!("Failed to set wallpaper crop mode!")
-  };
-  return Ok(());
+    println!("Refreshing wallpaper in function...");
+    let client = reqwest::Client::new();
+    let dl_url = new_wallpaper_url(&client).await.unwrap();
+    let file_name = dl_url.split("/").collect::<Vec<&str>>();
+    let image_file = cache_dir().unwrap().join(file_name[file_name.len() - 1]);
+    let downloaded_file = download_file_from_url(&client, dl_url, image_file)
+        .await
+        .unwrap();
+    wallpaper::set_from_path(downloaded_file.display().to_string().as_str()).unwrap();
+    match wallpaper::set_mode(wallpaper::Mode::Crop) {
+        Ok(()) => (),
+        Err(_) => eprintln!("Failed to set wallpaper crop mode!"),
+    };
+    return Ok(());
 }
