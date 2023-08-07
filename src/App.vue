937160f47e77ac4a8d83960ef84e0054a7f38d09
Wind-Explorer <66894537+Wind-Explorer@users.noreply.github.com>
2023-08-07 23:51:40 +0800

    Prepare for release

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import AppConfigurationPane from './views/AppConfigurationView.vue';
+
+document.addEventListener('contextmenu', event => event.preventDefault());
 </script>
 
 <template>
